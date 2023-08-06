3932b03cfa082a766fb4b07496c29cbec6728666
Wendigo43 <carlosmariomanchego1@gmail.com>
2023-08-06 18:56:12 -0500

    Feature-CalendarioDeEventos-CMMP

---
+++ Paginas principales/js/CalendarioDeEventos.js
@@ -0,0 +1,93 @@
+var eventos = [
+    {
+        "id": 1,
+        "sala": "Sala 1",
+        "organizador": "Carlos Mario Manchego Pabón",
+        "documentoOrganizador": 111,
+        "titulo": "Celebración de fin de año",
+        "horainicio": "20:50",
+        "horaFin": "22:30",
+        "fecha": "2023-12-31"
+    },
+    {
+        "id": 2,
+        "sala": "Sala 1",
+        "organizador": "Sebastian Duerr",
+        "documentoOrganizador": 222,
+        "titulo": "Celebración de cumpleaños",
+        "horainicio": "8:00",
+        "horaFin": "9:30",
+        "fecha": "2023-12-31"
+    },
+    {
+        "id": 3,
+        "sala": "Sala 1",
+        "organizador": "Masayoshi Fukumoto",
+        "documentoOrganizador": 333,
+        "titulo": "Celebración de BabyShower",
+        "horainicio": "10:00",
+        "horaFin": "13:00",
+        "fecha": "2023-12-31"
+    },
+    {
+        "id": 4,
+        "sala": "Sala 2",
+        "organizador": "Runa Iida",
+        "documentoOrganizador": 444,
+        "titulo": "Celebración de 15 años",
+        "horainicio": "10:00",
+        "horaFin": "18:00",
+        "fecha": "2023-12-31"
+    },
+    {
+        "id": 5,
+        "sala": "Sala 2",
+        "organizador": "Seymour Labrecque",
+        "documentoOrganizador": 555,
+        "titulo": "Funeral",
+        "horainicio": "20:20",
+        "horaFin": "22:40",
+        "fecha": "2023-12-31"
+    },
+    {
+        "id": 6,
+        "sala": "Sala 2",
+        "organizador": "Eric Lecarde",
+        "documentoOrganizador": 666,
+        "titulo": "Curso: Manejo de lanzas",
+        "horainicio": "6:00",
+        "horaFin": "9:20",
+        "fecha": "2023-12-31"
+    },
+    {
+        "id": 7,
+        "sala": "Sala 3",
+        "organizador": "Trevor Belmont",
+        "documentoOrganizador": 777,
+        "titulo": "Curso: Defenza contra demonios con latigo",
+        "horainicio": "6:00",
+        "horaFin": "9:20",
+        "fecha": "2023-12-31"
+    },
+    {
+        "id": 8,
+        "sala": "Sala 3",
+        "organizador": "Sypha Velnades",
+        "documentoOrganizador": 888,
+        "titulo": "Curso: Defenza contra demonios con magia arcana",
+        "horainicio": "10:00",
+        "horaFin": "14:50",
+        "fecha": "2023-12-31"
+    },
+    {
+        "id": 9,
+        "sala": "Sala 3",
+        "organizador": "Adrian Tepes",
+        "documentoOrganizador": 999,
+        "titulo": "Los humanos no son comida",
+        "horainicio": "15:00",
+        "horaFin": "17:30",
+        "fecha": "2023-12-31"
+    },
+    
+]
