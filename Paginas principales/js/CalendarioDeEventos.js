f324b555c06bbdf7fb3ae2111df9e057c8ebba72
Wendigo43 <carlosmariomanchego1@gmail.com>
2023-08-06 19:34:22 -0500

    Update-CalendarioDeEventos-CMMP

--- Paginas principales/js/CalendarioDeEventos.js
+++ Paginas principales/js/CalendarioDeEventos.js
@@ -89,5 +89,94 @@ var eventos = [
         "horaFin": "17:30",
         "fecha": "2023-12-31"
     },
-    
+    {
+        "id": 10,
+        "sala": "Sala 1",
+        "organizador": "Edward Buck",
+        "documentoOrganizador": 101010,
+        "titulo": "Como ganarle a un Brute",
+        "horainicio": "6:00",
+        "horaFin": "9:00",
+        "fecha": "2023-12-30"
+    },
+    {
+        "id": 11,
+        "sala": "Sala 1",
+        "organizador": "Ricther Belmont",
+        "documentoOrganizador": 111111,
+        "titulo": "Rezar por otros",
+        "horainicio": "13:00",
+        "horaFin": "15:00",
+        "fecha": "2023-12-30"
+    },
+    {
+        "id": 12,
+        "sala": "Sala 1",
+        "organizador": "Jericho Swain",
+        "documentoOrganizador": 121212,
+        "titulo": "Dirige a las masas, se un lider",
+        "horainicio": "17:00",
+        "horaFin": "19:00",
+        "fecha": "2023-12-30"
+    },
+    {
+        "id": 13,
+        "sala": "Sala 2",
+        "organizador": "Steve Rogers",
+        "documentoOrganizador": 131313,
+        "titulo": "Lanzamientos de escudo",
+        "horainicio": "5:00",
+        "horaFin": "8:00",
+        "fecha": "2023-12-30"
+    },
+    {
+        "id": 14,
+        "sala": "Sala 2",
+        "organizador": "Mario Mario",
+        "documentoOrganizador": 141414,
+        "titulo": "De destapar caños, a salvar un reino",
+        "horainicio": "9:00",
+        "horaFin": "12:00",
+        "fecha": "2023-12-30"
+    },
+    {
+        "id": 15,
+        "sala": "Sala 2",
+        "organizador": "Clark Ken",
+        "documentoOrganizador": 151515,
+        "titulo": "El arte del periodismo",
+        "horainicio": "13:00",
+        "horaFin": "15:00",
+        "fecha": "2023-12-30"
+    },
+    {
+        "id": 16,
+        "sala": "Sala 3",
+        "organizador": "Marco Aurelio",
+        "documentoOrganizador": 161616,
+        "titulo": "Hasta los emperadores se levantan de su cama",
+        "horainicio": "6:00",
+        "horaFin": "8:00",
+        "fecha": "2023-12-30"
+    },
+    {
+        "id": 17,
+        "sala": "Sala 3",
+        "organizador": "Vlad Dracula Tepes",
+        "documentoOrganizador": 171717,
+        "titulo": "Sabiduria milenaria",
+        "horainicio": "9:00",
+        "horaFin": "11:00",
+        "fecha": "2023-12-30"
+    },
+    {
+        "id": 18,
+        "sala": "Sala 3",
+        "organizador": "Thais Pabón",
+        "documentoOrganizador": 181818,
+        "titulo": "Maternidad: un arte",
+        "horainicio": "12:00",
+        "horaFin": "14:00",
+        "fecha": "2023-12-30"
+    },
 ]
